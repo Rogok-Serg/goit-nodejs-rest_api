afaa4dc295b438171f345335296ca5632671e46c
Rogok-Serg <rogoksv@gmail.com>
2023-09-27 18:16:16 +0300

    create test

---
+++ routes/api/auth-router.test.js
@@ -0,0 +1,46 @@
+import mongoose from "mongoose";
+import request from "supertest";
+import app from "../../app.js";
+import "dotenv/config.js";
+
+import User from "../../models/Users.js";
+
+const { DB_HOST, PORT } = process.env;
+
+describe("test login route", () => {
+  let server = "";
+
+  beforeAll(async () => {
+    await mongoose.connect(DB_HOST);
+    server = app.listen(PORT);
+  });
+
+  afterAll(async () => {
+    await mongoose.connection.close();
+    server.close();
+  });
+
+  beforeEach(() => {});
+
+  afterEach(async () => {
+    // await User.deleteMany({});
+  });
+
+  test("test login with correct data", async () => {
+    const loginData = {
+      email: "svetilnick.svetliy@gmail.com",
+      password: "7777777",
+    };
+    const { statusCode, body } = await request(app)
+      .post("/api/users/login")
+      .send(loginData);
+    const user = await User.findOne({ email: loginData.email });
+
+    expect(statusCode).toBe(201);
+    expect(body.user.email).toBe(loginData.email);
+
+    expect(body.token).toBe(user.token);
+    expect(body.user.subscription).toBe(user.subscription);
+    expect(user.email).toBe(loginData.email);
+  });
+});
